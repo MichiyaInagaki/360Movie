50c8b61dc0d5b5a948bce959e9d6b07637e688f4
MichiyaInagaki <inagaki@sens.sys.es.osaka-u.ac.jp>
2020-09-11 15:09:54 +0900

    add UDP

--- UDP.cs
+++ UDP.cs
@@ -10,6 +10,11 @@ public class UDP : MonoBehaviour
 {
     public float yaw_val;
     public float pitch_val;
+    private float pre_yaw = 0.0f;
+    private float pre_pitch = 0.0f;
+    private float filter_gain = 0.5f;
+    private float initial_yaw = 0.0f;
+    private float initial_pitch = 0.0f;
     //
     static UdpClient udp;
     IPEndPoint remoteEP = null;
@@ -32,6 +37,18 @@ public class UDP : MonoBehaviour
         string[] dest = text.Split(',');                //データごとに分割
         yaw_val = -float.Parse(dest[0]);
         pitch_val = float.Parse(dest[1]);
+        // ローパスフィルタ
+        //yaw_val = pre_yaw * filter_gain + yaw_val * (1 - filter_gain);
+        //pre_yaw = yaw_val;
+        //pitch_val = pre_pitch * filter_gain + pitch_val * (1 - filter_gain);
+        //pre_pitch = pitch_val;
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            initial_yaw = yaw_val;
+            initial_pitch = pitch_val;
+        }
+        yaw_val = yaw_val - initial_yaw;
+        pitch_val = pitch_val - initial_pitch;
         Debug.Log("yaw: " + yaw_val +" pitch: " + pitch_val);					//送られてきたデータの表示
     }
 }
